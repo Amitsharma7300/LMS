277e976b48c886511196f03abdbe5eccfde1b470
Amit sharma <sharmaamit9627172414@gmail.com>
2025-03-27 10:57:30 +0530

    Server correction

--- server/server.js
+++ server/server.js
@@ -21,6 +21,5 @@ connectDB().then(() => {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch((error) => {
-  console.error(`Error connecting to the database: ${error}`);
-  process.exit(1); // Exit process with failure
+  console.error(`Error connecting to the database: ${error}`); // 
 });
