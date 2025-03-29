0c82f714ddd136c9a8e4d3eca1035e83c2c2d30d
Amit sharma <sharmaamit9627172414@gmail.com>
2025-03-29 12:01:51 +0530

    backend done

--- server/server.js
+++ server/server.js
@@ -19,25 +19,27 @@ await connectCloudinary()
 // Connect to MongoDB
 connectDB()
   .then(() => {
-  app.use(express.json());
-  app.use(cors());
-  app.use(clerkMiddleware())
+    app.use(express.json());
+    app.use(cors());
+    app.use(clerkMiddleware());
 
-  // Your routes and middleware here
+    // Your routes and middleware here
 
-  app.get('/', (req, res) => res.send("Api working"));
-  app.post('/clerk', express.json(),clerkWebhooks)
-  app.use('/api/educator',express.json(),educatorRouter)
-  app.post('/api/educator/update-role', updateRoleToEducator);
-  app.use('/api/course',express.json(),courseRouter)
-  app.use('/api/user',express.json(),userRouter)
-app.post('/stripe',express.raw({type:'application/json'}),StripeWebhooks)
+    app.get("/", (req, res) => res.send("Api working"));
+    app.post("/clerk", express.json(), clerkWebhooks);
+    app.use("/api/educator", express.json(), educatorRouter);
+    app.post("/api/educator/update-role", updateRoleToEducator);
+    app.use("/api/course", express.json(), courseRouter);
+    app.use("/api/user", express.json(), userRouter);
+    app.post(
+      "/stripe",
+      express.raw({ type: "application/json" }),
+      StripeWebhooks
+    );
 
-
-
-
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}).catch((error) => {
-  console.error(`Error connecting to the database: ${error.message}`); // 
-});
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.error(`Error connecting to the database: ${error.message}`); //
+  });
