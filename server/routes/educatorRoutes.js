098fbf5976f76823faa46137cf2679155c54ef6f
Amit sharma <sharmaamit9627172414@gmail.com>
2025-04-17 12:51:18 +0530

    webhooks added

--- server/routes/educatorRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {updateRoleToEducator } from '../controllers/educatorController.js'
-
-
-const educatorRouter=express.Router()
-
-//add educator Role
-educatorRouter.get('/update-role',updateRoleToEducator)
-export default educatorRouter;
