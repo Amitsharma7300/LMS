c94f1e4c884cc9593545ce50659c377f115f1fe0
Amit sharma <sharmaamit9627172414@gmail.com>
2025-04-18 14:54:48 +0530

    server added

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from "./configs/mongodb.js"
+import { clerkWebhooks } from "./controllers/webhooks.js"
+
+const app=express()
+await connectDB()
+
+//middlewares
+app.use(cors())
+
+//routes
+app.get('/',(req,res)=>res.send('api is running'))
+app.post('/clerk',express.json(),clerkWebhooks)
+
+//port
+
+const PORT=process.env.PORT|| 5000;
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+})
+
